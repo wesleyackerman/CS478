1c9a8e144052ad2323d102a470380f65896dc1b8
Wesley Ackerman <wesley.ackerman@gmail.com>
2016-02-16 22:23:54 -0700

    Created NeuralNet class

---
+++ src/neuralnet/NeuralNet.java
@@ -0,0 +1,26 @@
+package neuralnet;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import toolkit.*;
+
+public class NeuralNet extends SupervisedLearner
+{
+	private Random rand;
+	private ArrayList<Double> weights;
+	private static final double LEARNING_RATE = .1;
+	
+	@Override
+	public void train(Matrix features, Matrix labels) throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void predict(double[] features, double[] labels) throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}	
+}
